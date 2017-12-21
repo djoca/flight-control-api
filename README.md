f46e2b28760d800373bfb54b9cec675fe70c3398
Jose Antonio Rosa dos Santos Junior <joses.jr@gmail.com>
2017-12-20 23:07:41 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Flight Control API
+
+This is a simple Flight Control Rest API made with Spring Boot. 
+
+## Getting started
+
+1. Make sure you have Git, Java 1.8 and Maven 3.5 installed.
+2. Clone this repository locally.
+3. Execute `mvn spring-boot:run` to start the server.
---
+++ src/main/java/net/j33r/flight/api/Boot.java
@@ -0,0 +1,12 @@
+package net.j33r.flight.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Boot.class, args);
+    }
+}
